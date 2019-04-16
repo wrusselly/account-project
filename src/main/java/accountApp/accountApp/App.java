74b586fae117a2ea16f46e9c26250e1a0358faca
wrusselly <w.russelly@gmail.com>
2019-04-16 10:49:06 +0100

    check conversion from map to JSON and back

--- src/main/java/accountApp/accountApp/App.java
+++ src/main/java/accountApp/accountApp/App.java
@@ -1,5 +1,6 @@
 package accountApp.accountApp;
 
+import java.util.Map;
 
 public class App 
 {
@@ -10,6 +11,17 @@ public class App
         
         Service.addAccount(a);
         Service.getAccount(1);
+        String accountString = Service.getJSONForObject(a);
+        System.out.println(accountString);
+        Account c = Service.getObjectForJSON(accountString, Account.class);
+        System.out.println(c);
+        
+        String mapString = Service.getJSONForObject(Service.accountMap);
+        System.out.println(mapString);
+        
+        Map<Integer, Account> map = Service.getObjectForJSON(mapString, Map.class);
+        System.out.println(map);
+        
         
         
     }
